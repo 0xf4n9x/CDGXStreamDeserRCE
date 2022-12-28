a7be5320c0e454c26b9969e46f66db58e56556f0
_0xf4n9x_ <fanqxu@gmail.com>
2022-12-29 03:09:14 +0800

    Update Decode Function

--- src/com/cdg/Decode.java
+++ src/com/cdg/Decode.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class Decode {
     public static void decoder(String targetstr) {
-//        System.out.println("请输入密文：");
 //        String var0 = "FEPCCCLCENHIPOAFPAPDDFCGEAPNMDBMOJPMJAKKNPHOKIKIDCBPHEGKLDGNHCBDEIMODEKMKPFBAIMMNLOJJKMIICLAPJAAFGNGAKFBMPKPJMOIKODEJJMHJCCHKBMFMMFDLOMDPABOJCEAPOFDCPMKGDHFNBBIMCIPAMMIIANFPAJHFAABLLLANNIDAGNKOHONJGFGBKHFDMCLJIMICBHBJEIAAIMACN";
         try {
             String result = ServiceUtil.decodeString(targetstr);
-            System.out.println("[+]Success:");
+            System.out.println("[+] Decode Successed:");
             System.out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
